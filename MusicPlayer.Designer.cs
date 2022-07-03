b4ef36c2cadc6991ab1a15e8c3f67db7a8f6e413
Alex Beazley <alexbeazley18@outlook.com>
2022-07-03 15:53:09 +1000

    Add project files.

---
+++ MusicPlayer.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace PianoMusicPlayer
+{
+    partial class MusicPlayer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label_TimeElapsed = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play C";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Click_PlayCMajor);
+            // 
+            // label_TimeElapsed
+            // 
+            this.label_TimeElapsed.AutoSize = true;
+            this.label_TimeElapsed.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label_TimeElapsed.Location = new System.Drawing.Point(156, 176);
+            this.label_TimeElapsed.Name = "label_TimeElapsed";
+            this.label_TimeElapsed.Size = new System.Drawing.Size(164, 32);
+            this.label_TimeElapsed.TabIndex = 1;
+            this.label_TimeElapsed.Text = "Time Elapsed";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_TimeElapsed);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label_TimeElapsed;
+    }
+}
---
+++ MusicPlayer.cs
@@ -0,0 +1,127 @@
+using NAudio.Wave;
+using PianoMusicPlayer.Helpers;
+using PianoMusicPlayer.Properties;
+using System.Timers;
+
+namespace PianoMusicPlayer
+{
+    public class ScaleStep
+    {
+        public double StepSize;
+        public Stream[] Notes;
+
+        public ScaleStep(double step, Stream[] notes)
+        {
+            StepSize = step;
+            Notes = notes;
+        }
+    }
+
+    public partial class MusicPlayer : Form
+    {
+        private System.Windows.Forms.Timer timer;
+        private DateTime timeStarted;
+        private TimeSpan timeElapsed;
+        private const int defaultInterval = 500;
+
+        public MusicPlayer()
+        {
+            InitializeComponent();
+            StartTimer();
+        }
+
+        public readonly Dictionary<string, ScaleStep> Steps = new Dictionary<string, ScaleStep>
+        {
+            { "A1", new ScaleStep(1, new [] {Resources.A, Resources.B}) },
+        };
+
+        private async void Click_PlayAMajor(object sender, EventArgs e)
+        {
+            await Play_NoteAsync(Resources.A, defaultInterval);
+            await Play_NoteAsync(Resources.B, defaultInterval);
+            await Play_NoteAsync(Resources.C, defaultInterval);
+            await Play_NoteAsync(Resources.D, defaultInterval);
+            await Play_NoteAsync(Resources.E, defaultInterval);
+            await Play_NoteAsync(Resources.F, defaultInterval);
+            await Play_NoteAsync(Resources.G, defaultInterval);
+            await Play_NoteAsync(Resources.A, defaultInterval);
+        }
+
+        private async void Click_PlayBMajor(object sender, EventArgs e)
+        {
+            await Play_NoteAsync(Resources.C, defaultInterval);
+            await Play_NoteAsync(Resources.D, defaultInterval);
+            await Play_NoteAsync(Resources.E, defaultInterval);
+            await Play_NoteAsync(Resources.F, defaultInterval);
+            await Play_NoteAsync(Resources.G, defaultInterval);
+            await Play_NoteAsync(Resources.A, defaultInterval);
+            await Play_NoteAsync(Resources.B, defaultInterval);
+            await Play_NoteAsync(Resources.C, defaultInterval);
+            await Play_NoteAsync(Resources.B, defaultInterval);
+            await Play_NoteAsync(Resources.A, defaultInterval);
+            await Play_NoteAsync(Resources.G, defaultInterval);
+            await Play_NoteAsync(Resources.F, defaultInterval);
+            await Play_NoteAsync(Resources.E, defaultInterval);
+            await Play_NoteAsync(Resources.D, defaultInterval);
+            await Play_NoteAsync(Resources.C, defaultInterval);
+        }
+
+
+        private async void Click_PlayCMajor(object sender, EventArgs e)
+        {
+            await Play_NoteAsync(Resources.C, defaultInterval);
+            await Play_NoteAsync(Resources.D, defaultInterval);
+            await Play_NoteAsync(Resources.E, defaultInterval);
+            await Play_NoteAsync(Resources.F, defaultInterval);
+            await Play_NoteAsync(Resources.G, defaultInterval);
+            await Play_NoteAsync(Resources.A, defaultInterval);
+            await Play_NoteAsync(Resources.B, defaultInterval);
+            await Play_NoteAsync(Resources.C1, defaultInterval);
+            await Play_NoteAsync(Resources.B, defaultInterval);
+            await Play_NoteAsync(Resources.A, defaultInterval);
+            await Play_NoteAsync(Resources.G, defaultInterval);
+            await Play_NoteAsync(Resources.F, defaultInterval);
+            await Play_NoteAsync(Resources.E, defaultInterval);
+            await Play_NoteAsync(Resources.D, defaultInterval);
+            await Play_NoteAsync(Resources.C, defaultInterval);
+        }
+        private async Task Play_NoteAsync(Stream stream, int interval_ms)
+        {
+            // Play audio sound
+            using (var player = new System.Media.SoundPlayer(stream))
+            {
+                player.Play();
+
+                // Wait until time has elapsed
+                await Task.Delay(interval_ms);
+                player.Stop();
+            }
+        }
+
+
+        private void StartTimer()
+        {
+            // Set time started to now
+            timeStarted = DateTime.Now;
+
+            // Create timer which updates every 10 milliseconds
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = (10);
+            timer.Tick += new EventHandler(UpdateTimer);
+
+            // Enable timer
+            timer.Enabled = true;
+        }
+
+        private void UpdateTimer(object sender, EventArgs e)
+        {
+            // Write time elapsed to label
+            timeElapsed = DateTime.Now.Subtract(timeStarted);
+            label_TimeElapsed.Text = timeElapsed.ToString("mm") + ":" + timeElapsed.ToString("ss") 
+                + "." + timeElapsed.ToString("ff");
+
+            // Render form changes
+            Application.DoEvents();
+        }
+    }
+}
